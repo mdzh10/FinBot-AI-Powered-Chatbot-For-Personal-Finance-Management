464dd99fcba62fca154e6816ca88920579fc6d9c
Ruthvik <142390372+ruthvikbairaboina@users.noreply.github.com>
2024-10-18 13:22:20 -0500

    created data models for Account schema,dashboard schema,Transaction schema

---
+++ backend/models/Account_schema.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from typing import Optional
+from enum import Enum
+
+
+class AccountTypeEnum(str, Enum):
+    cash = "cash"
+    bank = "bank"
+
+
+class AccountCreate(BaseModel):
+    user_id: int
+    account_type: AccountTypeEnum
+    bank_name: Optional[str] = None  # Applicable for bank accounts
+    account_name: str
+    account_number: Optional[str] = None  # Bank account number
+    routing_number: Optional[str] = None  # Bank routing number
+    balance: Optional[float] = 0.0  # Initial balance
+
+
+class AccountResponse(BaseModel):
+    id: int
+    user_id: int
+    account_type: AccountTypeEnum
+    bank_name: Optional[str]
+    account_name: str
+    account_number: Optional[str]
+    routing_number: Optional[str]
+    balance: float

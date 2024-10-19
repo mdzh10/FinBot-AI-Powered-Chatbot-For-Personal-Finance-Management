66e7a008d21da86c2fde5bb0a5796a9de40fad6e
mdzh10 <mdzh1997@gmail.com>
2024-10-19 03:13:51 -0500

    account router added in main.py

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from controllers.authentication_controller import router as auth_router
 from controllers.dashboard_controller import router as dashboard_router
 from controllers.transaction_controller import router as txn_router
+from controllers.account_controller import router as acc_router
 from db.database import database, create_tables
 
 app = FastAPI()
@@ -19,6 +20,7 @@ app.add_middleware(
 app.include_router(auth_router, prefix="/auth")
 app.include_router(dashboard_router, prefix="/dashboard")
 app.include_router(txn_router, prefix="/transaction")
+app.include_router(acc_router, prefix="/account")
 
 @app.on_event("startup")
 async def startup():

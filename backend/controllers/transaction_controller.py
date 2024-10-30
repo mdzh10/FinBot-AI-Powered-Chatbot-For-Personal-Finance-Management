34a4bdf116bfbb0555d65c6a5067cd72c817a8db
mdzh10 <mdzh1997@gmail.com>
2024-10-30 00:39:17 -0500

    add and modify transaction apis added

--- backend/controllers/transaction_controller.py
+++ backend/controllers/transaction_controller.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from config.db.database import get_db
-from services.transaction_service import get_all_transactions
-from schemas.transaction_schema import TransactionResponse
+from services.transaction_service import add_transaction, get_all_transactions, update_transaction
+from schemas.transaction_schema import TransactionResponse, TransactionCreate
 from typing import List
+from fastapi import HTTPException
 
 router = APIRouter()
 
@@ -16,3 +17,20 @@ async def get_transactions(user_id: int, db: Session = Depends(get_db)):
         return {"message": "User not found", "error_code": 404}
 
     return transactions
+
+@router.post("/add")
+async def create_transaction(transaction: TransactionCreate, db: Session= Depends(get_db)):
+
+    try:
+        new_transaction=add_transaction(db, transaction)
+        return await new_transaction
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+@router.post("/modify/{transaction_id}")  
+async def modify_transaction(transaction_id: int, transaction: TransactionCreate, db: Session=Depends(get_db)):
+    try:
+        modified_transaction=await update_transaction(db, transaction_id,transaction)
+        return modified_transaction
+    except Exception as e:
+        raise HTTPException(status_code=400, details=str(e))

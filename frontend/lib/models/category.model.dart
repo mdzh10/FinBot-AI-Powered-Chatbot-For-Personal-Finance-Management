828b8bbf4aacd2389c30ed1bf5e963678452835f
rifat-22 <rifatfahmida00@gmail.com>
2024-11-11 13:51:56 -0600

    category model updated

--- frontend/lib/models/category.model.dart
+++ frontend/lib/models/category.model.dart
@@ -3,16 +3,12 @@ import 'package:flutter/material.dart';
 class Category {
   int? id;
   String name;
-  IconData icon;
-  Color color;
   double? budget;
   double? expense;
 
   Category({
     this.id,
     required this.name,
-    required this.icon,
-    required this.color,
     this.budget,
     this.expense
   });
@@ -20,8 +16,6 @@ class Category {
   factory Category.fromJson(Map<String, dynamic> data) => Category(
     id: data["id"],
     name: data["name"],
-    icon: IconData(data["icon"], fontFamily: 'MaterialIcons'),
-    color: Color(data["color"]),
     budget: data["budget"] ?? 0,
     expense: data["expense"] ?? 0,
   );

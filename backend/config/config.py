a8e09f39bc90ccebbc45ca218a699bf160f97a25
mdzh10 <mdzh1997@gmail.com>
2024-11-27 13:26:14 -0600

    API key and DB URL is not mounted with secret volumes

--- backend/config/config.py
+++ backend/config/config.py
@@ -6,30 +6,20 @@ print(os.getcwd())
 # Load .env only for local development
 load_dotenv(dotenv_path="./config/.env", override=True, verbose=True)
 
-# class Settings:
-#     if os.path.exists("./config/secrets/DATABASE_URL"):
-#         DATABASE_URL: str = (
-#         open("./config/secrets/DATABASE_URL").read().strip()
-#     )
-#         print("got db url from docker")
-#     else:
-#         DATABASE_URL: str = (os.getenv("DATABASE_URL"))
-
-#     if os.path.exists("./config/secrets/GPT4_API_KEY"):
-#         GPT4_API_KEY: str = (
-#             open("./config/secrets/GPT4_API_KEY").read().strip()
-#         )
-#         print("got gpt key from docker")
-
-#     else:
-#         GPT4_API_KEY: str = (
-#             os.getenv("GPT4_API_KEY")
-#         )
-
 
 class Settings:
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    GPT4_API_KEY = os.getenv("GPT4_API_KEY")
+    if os.path.exists("/var/secrets/DB/DATABASE_URL"):
+        DATABASE_URL: str = open("/var/secrets/DB/DATABASE_URL").read().strip()
+        print("got db url from volume")
+    else:
+        DATABASE_URL: str = os.getenv("DATABASE_URL")
+
+    if os.path.exists("/var/secrets/GPT/GPT4_API_KEY"):
+        GPT4_API_KEY: str = open("/var/secrets/GPT/GPT4_API_KEY").read().strip()
+        print("got gpt key from volume")
+
+    else:
+        GPT4_API_KEY: str = os.getenv("GPT4_API_KEY")
 
 
 settings = Settings()

a4ef3bca4d4e78b9794121ffb98add91dc9339d8
rifat-22 <rifatfahmida00@gmail.com>
2024-11-27 02:13:59 -0600

    config and dockerfiles updated

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,13 +1,28 @@
-import os
 from dotenv import load_dotenv
+import os
 
-# Load environment variables from .env file
-load_dotenv()
-
+# Load .env only for local development
+load_dotenv(dotenv_path="./config/.env")
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
-    GPT4_API_KEY: str = os.getenv("GPT4_API_KEY")
+    if os.path.exists("./config/secrets/DATABASE_URL"):
+        DATABASE_URL: str = (
+        open("./config/secrets/DATABASE_URL").read().strip()
+    )
+        print("got db url from docker")
+    else:
+        DATABASE_URL: str = (os.getenv("DATABASE_URL"))
+
+    if os.path.exists("./config/secrets/GPT4_API_KEY"):
+        GPT4_API_KEY: str = (
+            open("./config/secrets/GPT4_API_KEY").read().strip()
+        )
+        print("got gpt key from docker")
+
+    else:
+        GPT4_API_KEY: str = (
+            os.getenv("GPT4_API_KEY")
+        )
 
 
 settings = Settings()
--- backend/config/db/database.py
+++ backend/config/db/database.py
@@ -2,9 +2,35 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine
 from databases import Database
 from config.config import settings
+import os
+from dotenv import load_dotenv
 
-database = Database(settings.DATABASE_URL)
-engine = create_engine(settings.DATABASE_URL)
+load_dotenv(dotenv_path="./config/.env")
+# Open the file in read mode
+file_path = './config/.env'
+
+try:
+    with open(file_path, 'r') as file:
+        # Read the lines of the file
+        file_lines = file.readlines()
+
+        # Print each line
+        print("File Content")
+        for line in file_lines:
+            print(line.strip())
+
+except FileNotFoundError:
+    print(f"File '{file_path}' not found")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+if settings is not None and settings.DATABASE_URL is not None:
+    database = Database(settings.DATABASE_URL)
+    engine = create_engine(settings.DATABASE_URL)
+else:
+    database = Database(os.getenv("DATABASE_URL"))
+    engine = create_engine(os.getenv("DATABASE_URL"))
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
